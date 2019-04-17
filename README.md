241f3fb13ac441dff5c5df4efe0f565375c8dac4
Bercovein <nlr_10_222@hotmail.com>
2019-04-17 17:03:27 -0300

    Inicio de Ahorcado

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# POC-Ahorcado
+La diferencia entre runnable y thread es que
+al extender de la clase thread 
+estamos limitando la herencia de la clase a la 
+que extiende ya que java no permite herencia 
+multiple. Sin embargo runnable se utiliza mediante 
+implementación lo que no interfiere en la herencia.
+
+Ciclo de vida de un thread:
+Un hilo tiene "vida" al ser ejecutado y muere
+cuando el metodo run termina su ejecución, cuando
+otro hilo llama a su metodo stop o el sistema operativo
+decide detenerlo  
+
+
+
+
+
+https://translate.google.com/translate?hl=es-419&sl=en&u=https://www.callicoder.com/java-multithreading-thread-and-runnable-tutorial/&prev=search
---
+++ ahorcado.sql
@@ -0,0 +1,43 @@
+create database ahorcado;
+
+use ahorcado;
+
+create table palabras(
+
+	id_palabra int auto_increment,
+    palabra varchar (50),
+    primary key (id_palabra)
+);
+
+insert into palabras (palabra) 
+	values ("interfaz"), ("estrategia"),
+	 ("clase"),("abstracto"), ("modularizacion"),
+     ("protocolo"), ("estatico"), ("polimorfia"),
+     ("herencia"), ("sangandrolfio");
+
+create table jugadores(
+
+	id_jugador int auto_increment,
+    nombre_jugador varchar(50),
+    primary key(id_jugador)
+);
+
+insert into jugadores (nombre_jugador) 
+	values ("Gianni"), ("Marco"),
+	 ("Marcos"),("Nicolas"), ("German"),
+     ("Pablo"), ("Guillermo"), ("Marcelo"),
+     ("Gustavo"), ("Paula"), ("Marito");
+
+
+create table resultados(
+
+    id_ganador int not null,
+    id_palabra int not null,
+    fecha date,
+    primary key(id_ganador,id_palabra),
+    foreign key (id_ganador) references jugadores(id_jugador),
+    foreign key (id_palabra) references palabras (id_palabra)
+);
+
+
+
